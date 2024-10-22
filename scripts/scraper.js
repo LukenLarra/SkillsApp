939d85db6051b79efb523cc97fd2185f918df386
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-10-22 17:21:08 +0200

    Add scraper script to fetch electronics skill tree data

---
+++ scripts/scraper.js
@@ -0,0 +1,7 @@
+const skills = fetch("https://tinkererway.dev/web_skill_trees/electronics_skill_tree")
+    .then(response => response.json())
+    .then(data => {
+        let id = data.id;
+        let text = data.text;
+        let icon = data.icon;
+    });
