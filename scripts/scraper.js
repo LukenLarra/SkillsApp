b65b18149e4d95188d651213b2b31444ee123bb7
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-10-25 17:48:14 +0200

    obtencion del texto e id

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,21 +1,33 @@
 import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
 
 const icons = [];
 const ids = [];
 const texts = [];
 
-export async function obtenerDatos(){
+async function obtenerDatos(){
     try {
         const response = await fetch("https://tinkererway.dev/web_skill_trees/electronics_skill_tree");
-        const data = await response.json();
-        data.forEach(item => {
-            icons.push(item.icon);
-            ids.push(item.id);
-            texts.push(item.text);
+        const data = await response.text();
+        const dom = new JSDOM(data);
+        const doc = dom.window.document;
+        const container = doc.querySelector(".svg-container");
+        const elements = container.querySelectorAll(".svg-wrapper");
+
+        elements.forEach((wrapper) => {
+            const svgImage = wrapper.querySelector("image");
+            const textoElement = wrapper.querySelector('text').textContent; // Selecciona el elemento text
+            const id = wrapper.getAttribute('data-id'); // Selecciona el atributo data-id
+
+            console.log('Enlace SVG:', svgImage);
+            console.log('Texto:', textoElement);
+            console.log('ID (data-id):', id);
         });
     } catch (error) {
-        console.error("Error al obtener los datos:", error);
+        console.log(error);
     }
 }
+
+obtenerDatos();
 
 export { icons, ids, texts };
