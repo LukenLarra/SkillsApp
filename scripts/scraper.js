67664b92ad4a0c576ad42fdb8b70ea8f50a33849
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-10-25 17:55:36 +0200

    Add scraper script to fetch electronics skill tree data

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,33 +1,38 @@
-import fetch from 'node-fetch';
-import { JSDOM } from 'jsdom';
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
 
 const icons = [];
 const ids = [];
 const texts = [];
 
-async function obtenerDatos(){
-    try {
-        const response = await fetch("https://tinkererway.dev/web_skill_trees/electronics_skill_tree");
-        const data = await response.text();
-        const dom = new JSDOM(data);
-        const doc = dom.window.document;
-        const container = doc.querySelector(".svg-container");
-        const elements = container.querySelectorAll(".svg-wrapper");
+async function obtenerDatos() {
+  try {
+    const response = await fetch(
+      "https://tinkererway.dev/web_skill_trees/electronics_skill_tree"
+    );
+    const data = await response.text();
+    const dom = new JSDOM(data);
+    const doc = dom.window.document;
+    const container = doc.querySelector(".svg-container");
+    const elements = container.querySelectorAll(".svg-wrapper");
 
-        elements.forEach((wrapper) => {
-            const svgImage = wrapper.querySelector("image");
-            const textoElement = wrapper.querySelector('text').textContent; // Selecciona el elemento text
-            const id = wrapper.getAttribute('data-id'); // Selecciona el atributo data-id
+    elements.forEach((wrapper) => {
+      const svgImage = wrapper.querySelector("image").getAttribute("href");
+      const rawText = wrapper.querySelector("text").textContent; // Selecciona el elemento text
+      const textElement = rawText.replace(/\s+/g, ' ');
+      const id = wrapper.getAttribute("data-id"); // Selecciona el atributo data-id
 
-            console.log('Enlace SVG:', svgImage);
-            console.log('Texto:', textoElement);
-            console.log('ID (data-id):', id);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+      icons.push(svgImage);
+      texts.push(textElement);
+      ids.push(id);
+    });
+
+    console.log("Íconos:", icons);
+    console.log("Textos:", texts);
+    console.log("IDs:", ids);
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-obtenerDatos();
 
 export { icons, ids, texts };
