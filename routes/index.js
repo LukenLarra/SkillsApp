f495c8746350397feb0d6ceb047e3989c484ea92
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-11-11 17:48:43 +0100

    index.html deleted

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Skills'});
+    res.render('index', {title: 'ELECTRONICS'});
 });
 
 
