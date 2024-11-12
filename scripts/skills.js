09b1e865dda579fe322f5c02a1dfc1d1610d66be
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-11-12 20:41:47 +0100

    added to the data more fields

--- scripts/skills.js
+++ scripts/skills.js
@@ -23,21 +23,21 @@ export function getDetails() {
 
 
 export async function setDetails(element){
-    buffer.resources = ['resource 1', 'resource 2', 'resource 3'];
+   /* buffer.resources = ['resource 1', 'resource 2', 'resource 3'];
     buffer.tasks = ['task 1', 'task 2', 'task 3'];
     buffer.description = 'This is a description of the skill';
     buffer.score = '1 points';
     buffer.title = element.querySelector('text').textContent;
-    buffer.svg = element.outerHTML;
+    buffer.svg = element.outerHTML;*/
 
-    /*const response = await fetch('http://localhost:3000/api/data');
+    const response = await fetch('http://localhost:3000/api/data');
     const data = await response.json();
     await Promise.all(data.map(async (item) => {
         buffer.resources = item.resources;
         buffer.tasks = item.tasks;
         buffer.description = item.description;
-        buffer.score = item.score;
-        buffer.title = item.text;
+        buffer.score = item.points;
     }));
-    buffer.svg = element.outerHTML;*/
+    buffer.title = element.querySelector('text').textContent;
+    buffer.svg = element.outerHTML;
 }
