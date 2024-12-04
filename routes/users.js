3cb4d3c01333e767007732d4fa6de8f0244159cc
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-12-04 15:51:40 +0100

    users.json registration method

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,59 @@
 import express from 'express';
-var router = express.Router();
+import path from "path";
+import {fileURLToPath} from "url";
+import fs from 'fs';
+
+let router = express.Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+    res.send('respond with a resource');
 });
 
-router.get('/leaderboard', function(req, res) {
-  res.render('leaderboard', { title: 'Range Explanations' });
+router.get('/leaderboard', function (req, res) {
+    res.render('leaderboard', {title: 'Range Explanations'});
 });
 
 router.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
 
 router.get('/register', (req, res) => {
-  res.render('register');
+    res.render('register');
+});
+
+router.post('/register', (req, res) => {
+    let password = req.body.password;
+    const username = req.body.username;
+    password = password[0];
+    const filePath = path.join(__dirname, '../users.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Error reading users file');
+        }
+
+        let users = [];
+        if (data) {
+            users = JSON.parse(data);
+        }
+
+        const role = users.length === 0 ? 'admin' : 'standard';
+        const newUser = {username, password, role};
+
+        users.push(newUser);
+
+        fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send('Error writing to users file');
+            }
+            res.redirect('/users/login');
+        });
+    });
 });
 
 export default router;
