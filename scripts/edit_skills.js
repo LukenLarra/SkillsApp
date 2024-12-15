b6dc2ca2c4aba8dbf9e708704da013113b5b1b91
Luken <llarranaga024@ikasle.ehu.eus>
2024-12-15 16:51:34 +0100

    icon preview option

--- scripts/edit_skills.js
+++ scripts/edit_skills.js
@@ -54,6 +54,7 @@ async function createSVGSkills() {
 function changeIcon() {
     const fileInput = document.getElementById('icon-upload');
     const fileNameDisplay = document.getElementById('file-name');
+    const iconPreview = document.getElementById('icon-preview');
     const MAX_SIZE = 1024 * 1024; // 1 MB
 
     fileInput.addEventListener('change', function () {
@@ -61,22 +62,34 @@ function changeIcon() {
 
         if (!file) {
             fileNameDisplay.textContent = 'No file selected';
+            iconPreview.src = '#';
+            iconPreview.classList.add('hidden');
             return;
         }
 
         if (file.type !== 'image/png') {
             fileNameDisplay.textContent = 'Only PNG files are allowed';
-            this.value = ''; // Reset input
+            this.value = '';
+            iconPreview.src = '#';
+            iconPreview.classList.add('hidden');
             return;
         }
 
         if (file.size > MAX_SIZE) {
             fileNameDisplay.textContent = 'File size must be less than 1 MB';
-            this.value = ''; // Reset input
+            this.value = '';
+            iconPreview.src = '#';
+            iconPreview.classList.add('hidden');
             return;
         }
 
         fileNameDisplay.textContent = file.name;
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            iconPreview.src = e.target.result;
+            iconPreview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
     });
 }
 
